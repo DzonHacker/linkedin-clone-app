2a407e2c401dd077590184956bb19cbb54856899
John Sherchan <john.sherchan10@gmail.com>
2021-03-21 11:30:45 +0545

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA9L4jeu588VZP1DYWNOyXrGZAjuG4pn7Q",
+    authDomain: "linkedin-clone-d82a7.firebaseapp.com",
+    projectId: "linkedin-clone-d82a7",
+    storageBucket: "linkedin-clone-d82a7.appspot.com",
+    messagingSenderId: "266681602186",
+    appId: "1:266681602186:web:5f00296bb11d65673fdd73"
+  };
